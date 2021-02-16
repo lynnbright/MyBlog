42a529c8416a1314da96b667dab513629cd77f51
Lynn Chang <tv725987@gmail.com>
2021-02-16 22:37:10 +0800

    feat: Add new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  before_action :find_options_for_article_status, only: [:new]
 
   def index
     @articles = Article.publish.order(created_at: :desc)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  private
+
+  def find_options_for_article_status
+    # TODO: _ use i18n to show select-text
+    @options_for_article_status = Article.statuses.map do |key, _|
+      [_, key]
+    end
   end
 end
