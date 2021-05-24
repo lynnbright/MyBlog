f78bc03ace236bb5fded6604b1ce7cf9a5cf2d1a
Lynn Chang <tv725987@gmail.com>
2021-05-24 20:25:06 +0800

    chore: Add todo comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
+# TODO: setup rails_helper file
+# require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
